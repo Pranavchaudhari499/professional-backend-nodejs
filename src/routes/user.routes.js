b9fb57ac264d5b016142f695a79ea8dd35fb033b
Pranav Chaudhari <pranavchaudhari499@gmail.com>
2026-01-20 13:08:10 +0530

    Refactor user routes to improve organization and readability by grouping related imports and endpoints

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,16 @@
 import {Router} from "express"
-import { loginUser, logoutUser, registerUser,refreshTokenAccess} from "../controllers/user.controller.js"
+import { loginUser, 
+        logoutUser, 
+        registerUser,
+        refreshTokenAccess,
+        changeCurrentPassword,
+        getCurrentUser,
+        updateAccountDetails,
+        updateUserAvatar,
+        updateUsercoverImage,
+        getUserChannelProfile, 
+        getWatchedhistory
+    } from "../controllers/user.controller.js"
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js"
 
@@ -24,5 +35,23 @@ router.route("/logout").post(verifyJWT, logoutUser)
 
 router.route("/refresh-route").post(refreshTokenAccess)
 
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"), updateUserAvatar)
+
+router.route("/cover-image")
+.patch(
+    verifyJWT,upload
+    .single("/coverImage"), 
+    updateUsercoverImage
+)
+
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+
+router.route("/history").get(verifyJWT,getWatchedhistory)
 
 export default router
