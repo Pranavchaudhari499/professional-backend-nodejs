cf4fd3811fc7eb4f065da92b503478b2a91799cf
Pranav Chaudhari <pranavchaudhari499@gmail.com>
2026-01-20 11:37:15 +0530

    Add getUserChannelProfile endpoint to fetch channel details and subscription status

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -335,6 +335,76 @@ const updateUsercoverImage = asyncHandler(async(req,res) => {
     )
 })
 
+const getUserChannelProfile = asyncHandler(async(req,res) => {
+    const {username} = req.params
+
+    if(!username?.trim()) {
+        throw new ApiError(400,"Username is missing")
+    }
+
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        {
+            $lookup:{
+                from: "subscriptions",
+                localField:"_id",
+                foreignField:"channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup:{
+                from: "subscriptions",
+                localField:"_id",
+                foreignField:"subscriber",
+                as: "subscribedto"
+            }
+        },
+        {
+            $addFields: {
+                subscribersCount: {
+                    $size: "subscribers"
+                },
+                channelsSubscribedtoCount: {
+                    $size: "subscribedTo"
+                },
+                isSubscribed:{
+                    $cond: {
+                        if: {$in:[req.user?._id,"$subscribers.subscriber"]},
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+        },
+        {
+            $project:{
+                fullName: 1,
+                username: 1,
+                subscribersCount: 1,
+                channelsSubscribedtoCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
+            }
+        }
+    ])
+
+    if(!channel?.length){
+        throw new ApiError(404,"channel does not found")
+    }
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,"Channel fetches succesfully")
+    )
+})
+
 export {registerUser,
         loginUser,
         logoutUser,
